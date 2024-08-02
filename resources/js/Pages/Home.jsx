31d44d2a79c54b7551e2be1846a5997d0331f4eb
jhonpaul37 <63507932+jhonpaul37@users.noreply.github.com>
2024-08-02 20:18:41 +0800

    Update Aug 02

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,3 +1,7 @@
 export default function Home() {
+<<<<<<< Updated upstream
     return <p className="bg-red-400">ni edit kuy ahahah</p>;
+=======
+    return <p className="bg-red-400">hello this is react js </p>;
+>>>>>>> Stashed changes
 }
