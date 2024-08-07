6017aa97e3f1a466b7a226b367c8c30e9d713a46
jhonpaul37 <63507932+jhonpaul37@users.noreply.github.com>
2024-08-07 22:03:16 +0800

    update Aug 7

--- resources/js/Pages/Voucher.jsx
+++ resources/js/Pages/Voucher.jsx
@@ -3,39 +3,42 @@ import { Link } from '@inertiajs/react';
 
 export default function Voucher({ vouchers }) {
     return (
-        <>
-            <div className="px-20 pt-10">
-                {/* button to add */}
-                <button className="rounded-md bg-high px-10 py-3 font-bold">
-                    Create
-                </button>
-                {/* display the recent*/}
-                <div className="my-10 bg-main text-white">
-                    <span>Recent</span>
-                    {vouchers.data.map((vouchers) => (
-                        <div key={vouchers.id} className="border bg-white p-4">
-                            <div>
-                                <span>
-                                    {new Date(
-                                        vouchers.created_at
-                                    ).toLocaleDateString()}
-                                </span>
-                            </div>
-                            <p>{vouchers.jev_no}</p>
+        <div className="px-20 pt-10">
+            {/* button to add */}
+            <button className="rounded-md bg-high px-10 py-3 font-bold">
+                Create
+            </button>
+
+            {/* display the recent*/}
+            <div className="my-10">
+                <div className="mb-2">Recent</div>
+                {vouchers.data.map((vouchers) => (
+                    <div key={vouchers.id} className="border bg-white p-4">
+                        <div>
+                            <span>
+                                {new Date(
+                                    vouchers.created_at
+                                ).toLocaleDateString()}
+                            </span>
                         </div>
-                    ))}
-                </div>
-                <div className="pt-5">
-                    {vouchers.links.map((link) => (
-                        <Link
-                            key={link.label}
-                            href={link.url}
-                            dangerouslySetInnerHTML={{ __html: link.label }}
-                            className="m-1 p-1"
-                        />
-                    ))}
-                </div>
+                        <p>{vouchers.jev_no}</p>
+                    </div>
+                ))}
             </div>
-        </>
+
+            <div className="pt-5">
+                {vouchers.links.map((link) => (
+                    <Link
+                        key={link.label}
+                        href={link.url}
+                        className={`${link.active ? 'font-bold text-high' : ''} m-1 p-1`}
+                    >
+                        <span
+                            dangerouslySetInnerHTML={{ __html: link.label }}
+                        />
+                    </Link>
+                ))}
+            </div>
+        </div>
     );
 }
