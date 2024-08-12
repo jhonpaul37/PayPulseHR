2ef27dc2fd9b20387f6e3ec1e2b3a4db162cfe84
jhonpaul37 <63507932+jhonpaul37@users.noreply.github.com>
2024-08-12 22:37:57 +0800

    update aug 12

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,13 +1,13 @@
-import "./bootstrap";
-import "../css/app.css";
-import Layout from "./layouts/layout";
+import './bootstrap';
+import '../css/app.css';
+import Layout from './layouts/layout';
 
-import { createInertiaApp } from "@inertiajs/react";
-import { createRoot } from "react-dom/client";
+import { createInertiaApp } from '@inertiajs/react';
+import { createRoot } from 'react-dom/client';
 
 createInertiaApp({
     resolve: (name) => {
-        const pages = import.meta.glob("./Pages/**/*.jsx", { eager: true });
+        const pages = import.meta.glob('./Pages/**/*.jsx', { eager: true });
         let page = pages[`./Pages/${name}.jsx`];
         page.default.layout =
             page.default.layout || ((page) => <Layout children={page} />);
--- resources/js/layouts/layout.jsx
+++ resources/js/layouts/layout.jsx
@@ -1,18 +1,27 @@
 import React, { useState } from 'react';
+
+// intertia
 import { Link, usePage } from '@inertiajs/react';
+
+// Font Awesome icon
 import {
-    MenuFoldOutlined,
-    MenuUnfoldOutlined,
-    UploadOutlined,
-    UserOutlined,
-    VideoCameraOutlined,
-} from '@ant-design/icons';
+    faHouse,
+    faGear,
+    faPlus,
+    faAddressBook,
+    faFile,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+// Ant design icon
+import { MenuFoldOutlined, MenuUnfoldOutlined } from '@ant-design/icons';
 import { Button, Layout, Menu } from 'antd';
-import styled from 'styled-components';
 
 const { Header, Sider, Content } = Layout;
 
-// // Styled components
+// Styled components
+import styled from 'styled-components';
+// this is the style
 const StyledSider = styled(Sider)`
     background-color: #741d20 !important;
 `;
@@ -54,14 +63,23 @@ const App = ({ children }) => {
                     selectedKeys={[selectedKey()]}
                     className="bg-main"
                 >
-                    <Menu.Item key="1" icon={<UserOutlined />}>
+                    <Menu.Item
+                        key="1"
+                        icon={<FontAwesomeIcon icon={faHouse} />}
+                    >
                         <Link href="/">Home</Link>
                     </Menu.Item>
-                    <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+                    <Menu.Item
+                        key="2"
+                        icon={<FontAwesomeIcon icon={faAddressBook} />}
+                    >
                         <Link href="/dashboard">Dashboard</Link>
                     </Menu.Item>
-                    <Menu.Item key="3" icon={<UploadOutlined />}>
+                    <Menu.Item key="3" icon={<FontAwesomeIcon icon={faFile} />}>
                         <Link href="/voucher">Voucher</Link>
+                    </Menu.Item>
+                    <Menu.Item key="4" icon={<FontAwesomeIcon icon={faGear} />}>
+                        <Link href="/settings">Settings</Link>
                     </Menu.Item>
                 </StyledMenu>
             </StyledSider>
