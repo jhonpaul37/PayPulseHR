cb74551aac1458db80ea2e8594d41932facb39d0
jhonpaul37 <63507932+jhonpaul37@users.noreply.github.com>
2024-09-03 23:10:32 +0800

    update sept. 3 v73

---
+++ resources/js/Pages/DashboardComponents/FundCluster.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const FundCluster = ({ fundClusters = [] }) => {
+    return (
+        <div>
+            <div className="rounded-md border p-3 shadow">
+                <h2 className="mb-3 font-bold">Balance Accounts</h2>
+
+                <div className="flex w-full justify-between gap-5">
+                    {fundClusters.length === 0 ? (
+                        <div>No data available</div>
+                    ) : (
+                        fundClusters.map((cluster) => (
+                            <div
+                                key={cluster.id}
+                                className="flex w-full flex-col rounded-md border bg-gray-100 p-2"
+                            >
+                                Total Balance
+                                <strong className="text-xl font-bold">
+                                    ₱{cluster.amount.toLocaleString()}
+                                </strong>
+                                <span>Cluster {cluster.cluster_code}</span>
+                            </div>
+                        ))
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+FundCluster.propTypes = {
+    fundClusters: PropTypes.array,
+};
+
+export default FundCluster;
