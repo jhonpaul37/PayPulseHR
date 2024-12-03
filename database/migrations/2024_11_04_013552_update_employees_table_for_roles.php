5a2cbeb80c782179943940cc845bd2096c09e4a8
jhonpaul37 <johnpaulblanca37@gmail.com>
2024-12-03 13:14:50 +0800

    Update 2024_11_04_013552_update_employees_table_for_roles.php

--- database/migrations/2024_11_04_013552_update_employees_table_for_roles.php
+++ database/migrations/2024_11_04_013552_update_employees_table_for_roles.php
@@ -12,9 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('employees', function (Blueprint $table) {
-            // Change the 'role' column to 'roles' as a JSON type
-            $table->dropColumn('role'); // Drop the old 'role' column
-            $table->json('roles')->nullable(); // Add new 'roles' column
+            $table->dropColumn('role');
+            $table->json('roles')->nullable();
         });
     }
 
@@ -24,8 +23,8 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('employees', function (Blueprint $table) {
-            $table->dropColumn('roles'); // Drop the 'roles' column
-            $table->string('role')->nullable(); // Re-add the 'role' column
+            $table->dropColumn('roles');
+            $table->string('role')->nullable();
         });
     }
 };
